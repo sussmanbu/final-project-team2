781ee5d6e19847e98a71e8a5e41c1fc25104fe1b
Chen Yang <arthuryc@bu.edu>
2024-04-01 15:24:16 -0400

    qqq

--- load_and_clean_data.R
+++ load_and_clean_data.R
@@ -5,7 +5,7 @@ police_data <- read_csv(here::here("dataset", "police_fatalities.csv"))
 #clean data
 police_data_clean <- police_data
 
-write_rds(police_data_clean, file = here::here("dataset", "police_refusal_clean.rds"))
+write_rds(police_data_clean, file = here::here("dataset", "police_clean.rds"))
 
-save(police_data_clean, file = here::here("dataset/police_refusal.RData"))
+save(police_data_clean, file = here::here("dataset/police.RData"))
 
