1605f83b20d396d3b7df816ed709d2261bc6b619
Carolyn Beigh <ccbeigh@bu.edu>
2024-04-11 17:54:27 -0400

    Built site for gh-pages

---
+++ load_and_clean_data.R
@@ -0,0 +1,11 @@
+#load data
+library(tidyverse)
+police_data <- read_csv(here::here("dataset", "police_fatalities.csv"))
+
+#clean data
+police_data_clean <- police_data
+
+write_rds(police_data_clean, file = here::here("dataset", "police_clean.rds"))
+
+save(police_data_clean, file = here::here("dataset/police.RData"))
+
