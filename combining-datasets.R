443d8602f4588ec9471c05b21a9074dfd2c1bc9f
Carolyn Beigh <ccbeigh@bu.edu>
2024-04-11 17:34:40 -0400

    Combined datasets and saved

---
+++ combining-datasets.R
@@ -0,0 +1,22 @@
+suppressPackageStartupMessages(library(tidyverse))
+
+police_f <- read.csv(here::here("dataset", "police_fatalities.csv"))
+
+us_dem <- read.csv(here::here("dataset", "us_cities_demographics.csv"), sep=";")
+
+# pivot so that there is only one observation for each city, 
+#   each race has its own column w/count
+us_dem_wid <- us_dem |> 
+  pivot_wider(names_from = "Race", values_from = "Count")
+
+# inner join to keep only the data that has a match from each dataset, 
+#   remove unnecessary State.y variable and rename State.x to State
+police_dem <- police_f |>
+  inner_join(us_dem_wid, join_by(City), relationship = "many-to-many") |>
+  select(-State.y) |> rename(State = State.x)
+
+# remove the observations with missing Race values as race is the main variable of interest
+police_dem <- police_dem |> filter(Race != "")
+
+# save combined dataset in dataset folder
+save(police_dem, file = here::here("dataset/police_demo_combined.RData"))
