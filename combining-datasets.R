fe310a5a9ebdd4690030f915eaf0f222fc24d609
Haotian Wu <hwu2024@bu.edu>
2024-04-24 15:17:40 -0400

    update combined_dataset

--- combining-datasets.R
+++ combining-datasets.R
@@ -2,6 +2,16 @@ suppressPackageStartupMessages(library(tidyverse))
 
 police_f <- read.csv(here::here("dataset", "police_fatalities.csv"))
 
+police_f <- police_f |>
+  filter(Race != "" & !is.na(Race)) |>
+  mutate(Race = recode(Race,
+                       "Black" = "Black or African-American",
+                       "White" = "White",
+                       "Hispanic" = "Hispanic or Latino",
+                       "Asian" = "Asian",
+                       "Native" = "American Indian and Alaska Native",
+                       "Other" = "Other"))
+
 us_dem <- read.csv(here::here("dataset", "us_cities_demographics.csv"), sep=";")
 
 # pivot so that there is only one observation for each city, 
